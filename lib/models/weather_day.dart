661d0d30ead809b5c1f44010c3ec708ce46e10e2
Aleksandr Schuchkin <jinmaster707@gmail.com>
2022-05-25 18:19:04 +0300

    WIP: Step 7

---
+++ lib/models/weather_day.dart
@@ -0,0 +1,16 @@
+class WeatherDay {
+  WeatherDay({
+    required this.dayName,
+    required this.icon,
+    required this.degrees,
+    required this.locationName,
+    required this.weatherDescription,
+  });
+
+  final String dayName; // день недели
+  final String icon; // путь до картинки, например Images.ic02d
+  final String degrees; // количество градусов
+  final String locationName; // название города населенного пункта
+  final String
+      weatherDescription; // какоя погода в этот день, например Солнечно
+}
